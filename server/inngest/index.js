1874b9164889f2c1f41f645c8816c789d659c11a
ParthNaykude <parthnaykude111222@gmail.com>
2025-12-20 10:03:30 +0530

    Backend updates: booking and email logic

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,6 +1,6 @@
 import { Inngest } from "inngest";
 import User from "../models/User.js";
-import sendEmail from "../configs/nodeMailer.js";
+// import sendEmail from "../configs/nodeMailer.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
@@ -48,34 +48,34 @@ const syncUserUpdation = inngest.createFunction(
 )
 
 // Inngest function to send email when user books a show
-const sendBookingConfirmationEmail = inngest.createFunction(
-    { id: "send-booking-confirmation-email" },
-    { event: "app/show.booked" },
-    async ({ event, step }) => {
-        const { bookingId } = event.data;
+// const sendBookingConfirmationEmail = inngest.createFunction(
+//     { id: "send-booking-confirmation-email" },
+//     { event: "app/show.booked" },
+//     async ({ event, step }) => {
+//         const { bookingId } = event.data;
 
-        const booking = await Booking.findById(bookingId).populate({
-            path: 'show',
-            populate: { path: "movie", model: "Movie" }
-        }).populate('user');
+//         const booking = await Booking.findById(bookingId).populate({
+//             path: 'show',
+//             populate: { path: "movie", model: "Movie" }
+//         }).populate('user');
 
-        await sendEmail({
-            to: booking.user.email,
-            subject: `Payment Confirmation: "${booking.show.movie.title} " booked! `,
-            body: `<div style="font-family: Arial, Helvetica, sans-serif; line-height: 1.5;">
-                        <h2>Hi ${booking.user.name},</h2>
-                         <p>Your booking for <strong style="color: #F84565;"> "${booking.show.movie.title}" </strong> is confirmed.</p>
-                            <p>
-                                <strong>Date:</strong>${new Date(booking.show.showDateTime).toLocalDateString('en-us', { timeZone: 'Asia/Kolkata' })} <br/>
-                                 Date(booking.show.showDateTime).toLocaleTimeString('en-us', { timeZone: 'Asia/Kolkata' })
-                             </p>
-                        <p>Enjoy the show!!</p>
-                        <p>Thanks for booking with us! <br/>--QuickShow Team--</p>
-                    </div>`
-        })
-    }
-)
+//         await sendEmail({
+//             to: booking.user.email,
+//             subject: `Payment Confirmation: "${booking.show.movie.title} " booked! `,
+//             body: `<div style="font-family: Arial, Helvetica, sans-serif; line-height: 1.5;">
+//                         <h2>Hi ${booking.user.name},</h2>
+//                          <p>Your booking for <strong style="color: #F84565;"> "${booking.show.movie.title}" </strong> is confirmed.</p>
+//                             <p>
+//                                 <strong>Date:</strong>${new Date(booking.show.showDateTime).toLocalDateString('en-us', { timeZone: 'Asia/Kolkata' })} <br/>
+//                                  Date(booking.show.showDateTime).toLocaleTimeString('en-us', { timeZone: 'Asia/Kolkata' })
+//                              </p>
+//                         <p>Enjoy the show!!</p>
+//                         <p>Thanks for booking with us! <br/>--QuickShow Team--</p>
+//                     </div>`
+//         })
+//     }
+// )
 
 
 // Create an empty array where we'll export future Inngest functions
-export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation, sendBookingConfirmationEmail];
+export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
